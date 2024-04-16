2047c75fa46f21f305162b3039b6313aa42a3f8b
John Madden <jmadden173@pm.me>
2024-04-16 00:00:33 -0700

    Allowed for fine grain control of lmhp packages

--- Middlewares/Third_Party/LoRaWAN/LmHandler/Packages/LmhpPackagesRegistration.c
+++ Middlewares/Third_Party/LoRaWAN/LmHandler/Packages/LmhpPackagesRegistration.c
@@ -26,6 +26,17 @@
 #include "LmhpFragmentation.h"
 #include "frag_decoder_if.h"
 #include "LmhpFirmwareManagement.h"
+#elif (defined (LORAWAN_DATA_DISTRIB_MGT) && (LORAWAN_DATA_DISTRIB_MGT == 0))
+#if (defined (LORAWAN_ENABLE_CLOCK_SYNC) && (LORAWAN_ENABLE_CLOCK_SYNC == 1))
+#include "LmhpClockSync.h"
+#endif
+#if (defined (LORAWAN_ENABLE_REMOTE_MCAST_SETUP) && (LORAWAN_ENABLE_REMOTE_MCAST_SETUP == 1))
+#include "LmhpRemoteMcastSetup.h"
+#endif
+#if (defined (LORAWAN_ENABLE_FRAGMENTATION) && (LORAWAN_ENABLE_FRAGMENTATION == 1))
+#include "LmhpFragmentation.h"
+#include "frag_decoder_if.h"
+#endif
 #endif /* LORAWAN_DATA_DISTRIB_MGT */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -61,6 +72,26 @@ LmHandlerErrorStatus_t LmhpPackagesRegistrationInit( Version_t *fwVersion )
         return LORAMAC_HANDLER_ERROR;
     }
 #endif /* LORAWAN_PACKAGES_VERSION */
+#elif (defined (LORAWAN_DATA_DISTRIB_MGT) && (LORAWAN_DATA_DISTRIB_MGT == 0))
+#if (defined (LORAWAN_ENABLE_CLOCK_SYNC) && (LORAWAN_ENABLE_CLOCK_SYNC == 1))
+    if( LmHandlerPackageRegister( PACKAGE_ID_CLOCK_SYNC, NULL ) != LORAMAC_HANDLER_SUCCESS )
+    {
+        return LORAMAC_HANDLER_ERROR;
+    }
+#endif
+#if (defined (LORAWAN_ENABLE_REMOTE_MCAST_SETUP) && (LORAWAN_ENABLE_REMOTE_MCAST_SETUP == 1))
+    else if( LmHandlerPackageRegister( PACKAGE_ID_REMOTE_MCAST_SETUP, NULL ) != LORAMAC_HANDLER_SUCCESS )
+    {
+        return LORAMAC_HANDLER_ERROR;
+    }
+#endif
+#if (defined (LORAWAN_ENABLE_FRAGMENTATION) && (LORAWAN_ENABLE_FRAGMENTATION == 1))
+    else if( LmHandlerPackageRegister( PACKAGE_ID_FRAGMENTATION,
+                                       ( LmhpFragmentationParams_t * )&FRAG_DECODER_IF_FragmentationParams ) != LORAMAC_HANDLER_SUCCESS )
+    {
+        return LORAMAC_HANDLER_ERROR;
+    }
+#endif
 #endif /* LORAWAN_DATA_DISTRIB_MGT */
 
     return LORAMAC_HANDLER_SUCCESS;
@@ -68,11 +99,11 @@ LmHandlerErrorStatus_t LmhpPackagesRegistrationInit( Version_t *fwVersion )
 
 LmHandlerErrorStatus_t LmhpPackagesRegister( uint8_t id, LmhPackage_t **package )
 {
-#if (defined (LORAWAN_DATA_DISTRIB_MGT) && (LORAWAN_DATA_DISTRIB_MGT == 1))
     if( package == NULL )
     {
         return LORAMAC_HANDLER_ERROR;
     }
+#if (defined (LORAWAN_DATA_DISTRIB_MGT) && (LORAWAN_DATA_DISTRIB_MGT == 1))
     switch( id )
     {
         case PACKAGE_ID_CLOCK_SYNC:
@@ -97,6 +128,33 @@ LmHandlerErrorStatus_t LmhpPackagesRegister( uint8_t id, LmhPackage_t **package 
             }
     }
     return LORAMAC_HANDLER_SUCCESS;
+#elif (defined (LORAWAN_DATA_DISTRIB_MGT) && (LORAWAN_DATA_DISTRIB_MGT == 0))
+    switch( id )
+    {
+#if (defined (LORAWAN_ENABLE_CLOCK_SYNC) && (LORAWAN_ENABLE_CLOCK_SYNC == 1))
+        case PACKAGE_ID_CLOCK_SYNC:
+            {
+                *package = LmhpClockSyncPackageFactory();
+                break;
+            }
+#endif
+#if (defined (LORAWAN_ENABLE_REMOTE_MCAST_SETUP) && (LORAWAN_ENABLE_REMOTE_MCAST_SETUP == 1))
+        case PACKAGE_ID_REMOTE_MCAST_SETUP:
+            {
+                *package = LmhpRemoteMcastSetupPackageFactory();
+                break;
+            }
+#endif
+#if (defined (LORAWAN_ENABLE_FRAGMENTATION) && (LORAWAN_ENABLE_FRAGMENTATION == 1))
+        case PACKAGE_ID_FRAGMENTATION:
+            {
+                *package = LmhpFragmentationPackageFactory();
+                break;
+            }
+#endif
+    }
+    return LORAMAC_HANDLER_SUCCESS;
+    
 #else
     return LORAMAC_HANDLER_ERROR;
 #endif /* LORAWAN_DATA_DISTRIB_MGT */
